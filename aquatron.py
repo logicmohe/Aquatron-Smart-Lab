966b059f27203b7811578999b8e5b3f42310def3
logicmohe <46504798+logicmohe@users.noreply.github.com>
2020-02-06 23:10:52 -0400

    Update aquatron.py

--- aquatron.py
+++ aquatron.py
@@ -87,12 +87,7 @@ class SettingScreen(Screen):
     #waiting for other items
     #could be wrong
     data_items=ListProperty([])
-
-    Clock.schedule_interval(self.get_users,0.1) #proper callback time, for now is 0.1 s
-    def get_users(self,dt):
-        global current_time
-        current_time=strftime("%Y-%m-%d %H:%M:%S",localtime())
-        self.ids.time_label.text=current_time
+    pass
 
 
 
