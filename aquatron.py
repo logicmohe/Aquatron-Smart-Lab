a2070d1a799153c369cc9a4092f4e10d9aa699dd
logicmohe <46504798+logicmohe@users.noreply.github.com>
2020-03-05 23:35:38 -0400

    why

--- aquatron.py
+++ aquatron.py
@@ -122,15 +122,15 @@ class StatisticScreen(Screen):
     data_items=ListProperty([])
     def __init__(self, **kwargs):
         super(StatisticScreen, self).__init__(**kwargs)
-        self.graph_test
+        self.graph_test()
         #Considering whether we should just do it updating each 10 mins
         #Clock.schedule_once(self.graph_test)
         #Clock.schedule_interval(self.graph_test,10) #proper callback time, for now is 0.1 s
     def graph_test(self,dt):
-        self.ids.line1.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-        self.ids.line1.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-        self.ids.line2.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-        self.ids.line2.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        self.ids.topline.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        self.ids.topline.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        self.ids.botline.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        self.ids.botline.add_widget(FigureCanvasKivyAgg(plt.gcf()))
         #return box
         #If this is keep refreshing, then use remove_widget(destination)
 
