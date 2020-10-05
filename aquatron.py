5236299034ca0f97d1600586bc94468b176ca50b
logicmohe <46504798+logicmohe@users.noreply.github.com>
2020-10-05 12:18:48 -0300

    Update aquatron.py

--- aquatron.py
+++ aquatron.py
@@ -87,11 +87,9 @@ class WaterSensorScreen(Screen):
         x = ['00:00','06:00','12:00','18:00']
 
         self.graph_generate()
-        fig=plt.figure(0)
-        ax=fig.add_subplot(1,1,1)
+        plt.figure(0)
 
         plt.plot(x,[17,19,23,20])
-        ax.xaxis.set_major_locator(2)
         plt.title('Average Water Temperature')
         self.ids.topline.add_widget(FigureCanvasKivyAgg(plt.gcf()))
         
